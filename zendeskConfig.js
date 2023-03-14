ad5a734eba1050acff95ecc225eb9bf87d8769fd
duyenlan151 <1660144.16ck1@gmail.com>
2023-03-14 11:16:01 +0700

    [WEB] add zendesk chat

---
+++ zendeskConfig.js
@@ -0,0 +1,3 @@
+// <!-- Start of sutrixhelp Zendesk Widget script -->
+<script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=b176f97b-7453-4dd9-b861-886b195f00ca"> </script>
+// <!-- End of sutrixhelp Zendesk Widget script -->
